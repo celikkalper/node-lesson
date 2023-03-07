c1245821b1d90baafcfd3c56955ef496d1fdeee2
celikkalper <88833171+celikkalper@users.noreply.github.com>
2023-03-08 02:43:34 +0300

    Node js Second lesson start

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-console.log("Hello World!");
-
-const slugify = require("slugify");
+import slugify from "slugify";
+import add from "./my-module.js";
 
 const title = slugify("some string lorem ipsum", "-");
 
 console.log(title);
+
+console.log(add(2, 4));
---
+++ my-module.js
@@ -0,0 +1,5 @@
+const topla = function (a, b) {
+  return a + b;
+};
+
+export default topla;
